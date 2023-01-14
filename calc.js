02d0b0e5f0a0093d404062c8e4fc9a1b37ba1249
Caleb Holdaway <cdholdaway@gmail.com>
2023-01-14 14:52:06 -0700

    Addition implementation working

--- calc.js
+++ calc.js
@@ -31,7 +31,7 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1 / num2);
 	}
 	if (mathSymbol === "%"){
-		console.log(num1 % num2);
+		console.log(Math.sqrt(num1));
 	}
 	// This line closes the connection to the command line interface.
 	reader.close()
